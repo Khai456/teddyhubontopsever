9b5f375bc9cacf82eda8b464fcfb448f172d45fa
Khai456 <leduchoangkhai@gmail.com>
2025-05-17 22:55:10 +0700

    Update teddyhubontopsever.lua

--- teddyhubontopsever.lua
+++ teddyhubontopsever.lua
@@ -1,3 +1,3 @@
 repeat task.wait() until game:IsLoaded() and game:GetService("Players") and game.Players.LocalPlayer and game.Players.LocalPlayer:FindFirstChild("PlayerGui")
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Teddyseetink/Haidepzai/refs/heads/main/TEDDYHUB-FREEMIUM"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Khai456/hopboss/refs/heads/main/hopboss"))()
